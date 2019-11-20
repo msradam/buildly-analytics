52738d9f339b7445922ca1aeaee401d5a554b1f4
gholcomb <griffinholcomb@gmail.com>
2019-11-20 08:19:41 -0500

    style changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from gitter import gitter
 import pandas as pd
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from github_analytics import retrieve_repo_data
+from pprint import pprint as pp
 
 if __name__ == '__main__':
     #Load in the credentials & authorize client
@@ -19,19 +20,32 @@ if __name__ == '__main__':
     Data Studio 3 '''
 
     #Sets gitter worksheet
-    gitter_worksheet = client.open("Buildly Analytics").get_worksheet(1)
+    # gitter_worksheet = client.open("Buildly Analytics").get_worksheet(1)
 
     #Gets gitter chat metadata
-    gitter_meta_data = gitter.get_chat_meta()
+    # gitter_meta_data = gitter.get_chat_meta()
 
     #Sets github worksheet
     github_worksheet = client.open("Buildly Analytics").get_worksheet(2)
 
-    #Gets github chat metadata
+    #Gets github chat data
     github_meta_data = retrieve_repo_data.get_github_meta()
+    github_view_data = retrieve_repo_data.modularize_view_data()
+    github_clone_data = retrieve_repo_data.modularize_clone_data()
 
-    #Writes to the gitter google sheet
-    set_with_dataframe(gitter_worksheet, gitter_meta_data )
+    #Writes user by frame
+    # by_user_frame = github_meta_data.groupby("user").size().to_frame().T
 
-    #Writes to the github google sheet
-    set_with_dataframe(github_worksheet, github_meta_data)
+
+    # Writes to the gitter google sheet
+    set_with_dataframe(gitter_worksheet, gitter_meta_data)
+
+    #Writes to the github metadata google sheet
+    github_worksheet.update_acell('A1', 'Github Meta Data')
+    set_with_dataframe(github_worksheet, github_meta_data, row=2)
+
+    github_worksheet.update_acell('M1', 'Github viewer Data')
+    set_with_dataframe(github_worksheet, github_view_data, row=2, col=13)
+
+    github_worksheet.update_acell('Q1', 'Github clone Data')
+    set_with_dataframe(github_worksheet, github_clone_data, row=2, col=17)
